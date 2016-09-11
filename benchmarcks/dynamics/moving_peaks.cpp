6704e7cae9892d3cfb21581a473d906bbd96bcbd
Rafael Vincence <rafaelvincence@gmail.com>
2016-09-11 18:03:59 -0300

    implement the moving peaks initializer and base functions

--- benchmarcks/dynamics/moving_peaks.cpp
+++ benchmarcks/dynamics/moving_peaks.cpp
@@ -1,18 +1,46 @@
 #include "moving_peaks.hpp"
 
-MovingPeaks::MovingPeaks(int dimension){
-    
+MovingPeaks::MovingPeaks(int dimension, int scenario){
+  this->scen = new Scenario(scenario);
+
+  // set initial height for all peaks 
+  if(scen->start_height != 0){
+    for(int i=0; i<scen->npeaks; i++)
+      peaks_height.push_back(scen->start_height);
+  }else{
+    for(int i=0; i<scen->npeaks; i++)
+      peaks_height.push_back(fRand(scen->min_height, scen->max_height));
+  }
+
+  // set initial width for all peaks 
+  if(scen->start_width != 0){
+    for(int i=0; i<scen->npeaks; i++)
+      peaks_width.push_back(scen->start_width);
+  }else{
+    for(int i=0; i<scen->npeaks; i++)
+      peaks_width.push_back(fRand(scen->min_width, scen->max_width));
+  }
+
+  // set initial position for all peaks
+  std::vector<double> tmp_pos;
+  for(int i=0; i<scen->npeaks; i++){
+    tmp_pos.clear();
+    for(int j=0; j<this->dimension; j++){
+      tmp_pos.push_back(fRand(scen->min_coord, scen->max_coord));
+      peaks_position.push_back(tmp_pos);
+    }
+  }
 }
 
 MovingPeaks::MovingPeaks(){}
 MovingPeaks::~MovingPeaks(){}
 
 double MovingPeaks::getUpperBound(int pos){
-	return 0.0;
+	return scen->max_coord;
 }
 
 double MovingPeaks::getLowerBound(int pos){
-	return 0.0;
+	return scen->min_coord;
 }
 
 double MovingPeaks::evaluateFitness(std::vector<double> solution){
@@ -46,4 +74,30 @@ bool MovingPeaks::fitnesIsBetter(double newFit, double oldFit){
 
 bool MovingPeaks::isMinimization(){
 	return false;
+}
+
+double MovingPeaks::cone(std::vector<double> individual, std::vector<double> position, double height, double width){
+  double value = 0.0;
+  for(int i=0; i<this->dimension; i++)
+    value += pow(individual[i] - position[i], 2);
+  return height - width * sqrt(value);
+}
+
+double MovingPeaks::sphere(std::vector<double> individual, std::vector<double> position, double height, double width){
+  double value = 0.0;
+  for(int i=0; i<this->dimension; i++)
+    value += pow(individual[i] - position[i], 2);
+  return height  * value;
+}
+
+double MovingPeaks::function1(std::vector<double> individual, std::vector<double> position, double height, double width){
+  double value = 0.0;
+  for(int i=0; i<this->dimension; i++)
+    value += pow(individual[i] - position[i], 2);
+  return height / (1 + width * value);
+}
+
+double MovingPeaks::fRand(double fMin, double fMax){
+  double f = (double)rand() / RAND_MAX;
+  return fMin + f * (fMax - fMin);
 }
--- benchmarcks/dynamics/moving_peaks.hpp
+++ benchmarcks/dynamics/moving_peaks.hpp
@@ -7,12 +7,13 @@
 
 // Source: https://github.com/DEAP/deap/blob/master/deap/benchmarks/movingpeaks.py
 #include "../problem.hpp"
+#include "../scenario.hpp"
 
 using namespace std;
 
 class MovingPeaks: public Problem {
   public:
-    MovingPeaks(int dimension);
+    MovingPeaks(int dimension, int scenario = 1);
     MovingPeaks();
     ~MovingPeaks();
 
@@ -25,8 +26,58 @@ class MovingPeaks: public Problem {
     bool fitnesIsBetter(double newFit, double oldFit);
     bool isMinimization();
 
+    double cone(std::vector<double> individual, std::vector<double> position, double height, double width);
+    double sphere(std::vector<double> individual, std::vector<double> position, double height, double width);
+    double function1(std::vector<double> individual, std::vector<double> position, double height, double width);
+
+    double fRand(double fMin, double fMax);
   private:
     double dimension;
+    Scenario * scen;
+    std::vector<double> peaks_height;
+    std::vector<double> peaks_width;
+    std::vector<std::vector<double>> peaks_position;
 };
 
 #endif
+
+// The Moving Peaks Benchmark is a fitness function changing over time. It
+// consists of a number of peaks, changing in height, width and location. The
+// peaks function is given by *pfunc*, wich is either a function object or a
+// list of function objects (the default is :func:`function1`). The number of
+// peaks is determined by *npeaks* (which defaults to 5). This parameter can
+// be either a integer or a sequence. If it is set to an integer the number
+// of peaks won't change, while if set to a sequence of 3 elements, the
+// number of peaks will fluctuate between the first and third element of that
+// sequence, the second element is the inital number of peaks. When
+// fluctuating the number of peaks, the parameter *number_severity* must be
+// included, it represents the number of peak fraction that is allowed to
+// change. The dimensionality of the search domain is *dim*. A basis function
+// *bfunc* can also be given to act as static landscape (the default is no
+// basis function). The argument *random* serves to grant an independent
+// random number generator to the moving peaks so that the evolution is not
+// influenced by number drawn by this object (the default uses random
+// functions from the Python module :mod:`random`). Various other keyword
+// parameters listed in the table below are required to setup the benchmark,
+// default parameters are based on scenario 1 of this benchmark.
+
+// =================== ============================= =================== =================== ======================================================================================================================
+// Parameter           :data:`SCENARIO_1` (Default)  :data:`SCENARIO_2`  :data:`SCENARIO_3`    Details
+// =================== ============================= =================== =================== ======================================================================================================================
+// ``pfunc``           :func:`function1`             :func:`cone`        :func:`cone`        The peak function or a list of peak function.
+// ``npeaks``          5                             10                  50                  Number of peaks. If an integer, the number of peaks won't change, if a sequence it will fluctuate [min, current, max].
+// ``bfunc``           :obj:`None`                   :obj:`None`         ``lambda x: 10``    Basis static function.
+// ``min_coord``       0.0                           0.0                 0.0                 Minimum coordinate for the centre of the peaks.
+// ``max_coord``       100.0                         100.0               100.0               Maximum coordinate for the centre of the peaks.
+// ``min_height``      30.0                          30.0                30.0                Minimum height of the peaks.
+// ``max_height``      70.0                          70.0                70.0                Maximum height of the peaks.
+// ``uniform_height``  50.0                          50.0                0                   Starting height for all peaks, if ``uniform_height <= 0`` the initial height is set randomly for each peak.
+// ``min_width``       0.0001                        1.0                 1.0                 Minimum width of the peaks.
+// ``max_width``       0.2                           12.0                12.0                Maximum width of the peaks
+// ``uniform_width``   0.1                           0                   0                   Starting width for all peaks, if ``uniform_width <= 0`` the initial width is set randomly for each peak.
+// ``lambda_``         0.0                           0.5                 0.5                 Correlation between changes.
+// ``move_severity``   1.0                           1.5                 1.0                 The distance a single peak moves when peaks change.
+// ``height_severity`` 7.0                           7.0                 1.0                 The standard deviation of the change made to the height of a peak when peaks change.
+// ``width_severity``  0.01                          1.0                 0.5                 The standard deviation of the change made to the width of a peak when peaks change.
+// ``period``          5000                          5000                1000                Period between two changes.
+// =================== ============================= =================== =================== ======================================================================================================================
