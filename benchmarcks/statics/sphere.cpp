6f24d6f0db0c14336c687bbbb57c4e12e6d1a009
Rafael Vincence <rafaelvincence@gmail.com>
2016-10-10 08:16:45 -0300

    create sphere problem

---
+++ benchmarcks/statics/sphere.cpp
@@ -0,0 +1,39 @@
+#include "sphere.hpp"
+
+Sphere::Sphere(int dimension){
+  this->dimension = dimension;
+}
+
+Sphere::Sphere() {}
+Sphere::~Sphere() {}
+
+double Sphere::getUpperBound(int pos){
+  return 100.0;
+}
+
+double Sphere::getLowerBound(int pos){
+  return -100.0;
+}
+
+double Sphere::evaluateFitness(std::vector<double> solution){
+
+  double top = 0;
+  unsigned short int i;
+  for (i = 0; i < this->dimension; i++)
+  {
+    top=top+solution[i]*solution[i];
+  }
+  return top;
+}
+
+std::string Sphere::getName(){
+  return "Sphere";
+}
+
+bool Sphere::fitnesIsBetter(double newFit, double oldFit){
+  return newFit<oldFit;
+}
+
+bool Sphere::isMinimization(){
+  return true;
+}
---
+++ benchmarcks/statics/sphere.hpp
@@ -0,0 +1,26 @@
+#ifndef _SPHERE_H_
+#define _SPHERE_H_
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+#include "../problem.hpp"
+
+using namespace std;
+
+class Sphere: public Problem {
+  public:
+    Sphere(int dimension);
+    Sphere();
+    ~Sphere();
+
+    double getUpperBound(int pos);
+    double getLowerBound(int pos);
+    double evaluateFitness(std::vector<double> solution);
+    std::string getName();
+    bool fitnesIsBetter(double newFit, double oldFit);
+    bool isMinimization();
+}; 
+
+#endif
